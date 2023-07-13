2470ef949e89dd74471cc08c356a56921efd0a72
paras-017 <paraschauhan0017@gmail.com>
2023-07-13 07:25:27 +0530

    .

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const jsonfile = require('jsonfile')
 const moment = require('moment')
 const simpleGit = require('simple-git')
 const FILE_PATH = './data.json'
-const DATE = moment().format()
+const DATE = moment().subtract(1,'d').format()
 const data = {
     data:DATE
 }
 jsonfile.writeFile(FILE_PATH, data)
 
-simpleGit().add([FILE_PATH]).commit(DATE,{'--date':DATE})
+simpleGit().add([FILE_PATH]).commit(DATE,{'--date':DATE}).push()
