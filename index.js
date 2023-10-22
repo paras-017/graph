0362bb4c80e26035c8a80965c65076f557fd3da6
paras-017 <paraschauhan0017@gmail.com>
2023-10-22 10:50:03 +0530

    5

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const moment = require('moment')
 const simpleGit = require('simple-git')
 
 const FILE_PATH = './data.json'
-const DATE = moment().subtract(6,'d').format()
+const DATE = moment().subtract(5,'d').format()
 
 const data = {
     data:DATE
